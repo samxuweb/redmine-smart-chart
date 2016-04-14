a9a859000870fb464760b74d1dcf012f1b68d2eb
samxu <samxuweb@163.com>
2016-04-14 11:04:49 +0800

    Chart Barchart to Linechart

--- app/controllers/smart_charts_controller.rb
+++ app/controllers/smart_charts_controller.rb
@@ -4,59 +4,74 @@ class SmartChartsController < ApplicationController
   before_action :checkPermission
   def index
     @date = Time.now
-    @type = 'department'
-    @datax, @datay, @title = depName, depIssuesNumber(issues), l(:department_chart)
+    @title = "All Issues"
+    @data = allIssuesNumber('all', @date)
+    @data[:rate] = []
+    for i in 0...@data[:datax].length do
+      @data[:rate][i] = (@data[:closed][i] == 0) ? 0 : (@data[:closed][i] * 100 / @data[:ongoing][i])
+    end
+  end
+
+  def showDepartments
+    @date = Time.now
+    @datax, @datay = depName, depIssuesNumber(issues(@date))
     @datay[:rate] = []
     for i in 0...@datax.length do
-      @datay[:rate][i] = (@datay[:closed][i] == 0) ? 0 : (@datay[:closed][i] * 100 / (@datay[:new][i] + @datay[:ongoing][i] + @datay[:closed][i]))
+      @datay[:rate][i] = (@datay[:closed][i] == 0) ? 0 : (@datay[:closed][i] * 100 / @datay[:ongoing][i])
     end
+    @data = Hash.new
+    @data[:datax] = @datax
+    @data[:ongoing] = @datay[:ongoing]
+    @data[:closed] = @datay[:closed]
+    @data[:rate] = @datay[:rate]
   end
 
   def show
     @date = Time.now
     @title = params[:dep]
-    @type = params[:dep]
-    @datax = depMember(params[:dep])
-    @datay = depMemberIssuesNumber(params[:dep], issues)
-    @datay[:rate] = []
-    for i in 0...@datax.length do
-      @datay[:rate][i] = (@datay[:closed][i] == 0) ? 0 : (@datay[:closed][i] * 100 / (@datay[:new][i] + @datay[:ongoing][i] + @datay[:closed][i]))
+    @data = allIssuesNumber('group', @date, params[:dep])
+    @members = depMember(params[:dep])
+    @data[:rate] = []
+    for i in 0...@data[:datax].length do
+      @data[:rate][i] = (@data[:closed][i] == 0) ? 0 : (@data[:closed][i] * 100 / @data[:ongoing][i] )
     end
-    render :action => 'index'
-  end
-
-  def top10
-    @date = Time.now
-    @title = l(:top_ten)
-    @type = 'top10'
-    @datax = topTenIssueOwner(issues)
-    @datay = topTenIssueNumber(issues)
-    @datay[:rate] = []
-    for i in 0...@datax.length do
-      @datay[:rate][i] = (@datay[:closed][i] == 0) ? 0 : (@datay[:closed][i] * 100 / (@datay[:new][i] + @datay[:ongoing][i] + @datay[:closed][i]))
-    end
-    render :action => 'index'
+#    render :action => 'index'
   end
 
   def showRecentMonth
-    @date = Time.new(params[:year], params[:month]).end_of_month
-    @type = params[:type]
-    if params[:type] == 'department'
-      @title = l(:department_chart) 
-      @datax = depName
-      @datay = depIssuesNumber(issues(@date))
-    elsif params[:type] == 'top10'
-      @title = l(:top_ten) 
-      @datax = topTenIssueOwner(issues(@date))
-      @datay = topTenIssueNumber(issues(@date))
-    else
-      @title = params[:type] 
-      @datax = depMember(params[:type])
-      @datay = depMemberIssuesNumber(params[:type], issues(@date))
+    month = params[:month]
+    year = params[:year]
+    if month == "0"
+      month = 12
+      year = year.to_i - 1
+    elsif month == "13"
+      month = 1
+      year = year.to_i + 1
     end
+    @date = Time.new(year, month).end_of_month
+    @datax, @datay = depName, depIssuesNumber(issues(@date))
     @datay[:rate] = []
     for i in 0...@datax.length do
-      @datay[:rate][i] = (@datay[:closed][i] == 0) ? 0 : (@datay[:closed][i] * 100 / (@datay[:new][i] + @datay[:ongoing][i] + @datay[:closed][i]))
+      @datay[:rate][i] = (@datay[:closed][i] == 0) ? 0 : (@datay[:closed][i] * 100 / @datay[:ongoing][i])
+    end
+    @data = Hash.new
+    @data[:datax] = @datax
+    @data[:ongoing] = @datay[:ongoing]
+    @data[:closed] = @datay[:closed]
+    @data[:rate] = @datay[:rate]
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
+  def showDetails
+    @date = Time.now
+    member = User.find(params[:member])
+    @title = member.lastname
+    @data = allIssuesNumber('user', @date, member.id)
+    @data[:rate] = []
+    for i in 0...@data[:datax].length do
+      @data[:rate][i] = (@data[:closed][i] == 0) ? 0 : (@data[:closed][i] * 100 / @data[:ongoing][i] )
     end
     respond_to do |format|
       format.js {}
@@ -77,20 +92,24 @@ class SmartChartsController < ApplicationController
     Issue.where(:tracker_id => 4).where("due_date < ? AND due_date > ?", date, date - 1.month)
   end
 
-  def topTen(issues_from)
-    issues_from.joins("LEFT JOIN #{User.table_name} on #{User.table_name}.id = #{Issue.table_name}.assigned_to_id").where("#{User.table_name}.status = 1 AND type = 'User'").where(:tracker_id => 4).select("assigned_to_id, count(assigned_to_id) as assignee").group("assigned_to_id").order("assignee DESC").limit(10).map(&:assigned_to_id)
-  end
-
-  def topTenIssueNumber(issues_from)
+  def allIssuesNumber(type, date, d = '')
     number = Hash.new
-    number[:new] = topTen(issues_from).collect {|user| issues_from.where(:assigned_to_id => user, :status => 1).count}
-    number[:ongoing] = topTen(issues_from).collect {|user| issues_from.where(:assigned_to_id => user, :status => [2,5]).count}
-    number[:closed] = topTen(issues_from).collect {|user| issues_from.where(:assigned_to_id => user, :status => [3,4,6]).count}
-    number
-  end
-
-  def topTenIssueOwner(issues_from)
-    owner = topTen(issues_from).collect { |user| %Q{<a href="#{issues_url(:set_filter => 1, :assigned_to_id => user)}">"#{User.find(user).lastname}"</a>}}
+    number[:datax], number[:new], number[:ongoing], number[:closed] =  [], [], [], []
+    9.downto 0 do |i| 
+      date_new = date - i.month
+      if type == "all"
+        issues_from = issues(date_new)
+      elsif type == "group"
+        issues_from = issues(date_new).where(:assigned_to_id => Group.find_by_lastname(d).users.where(:status => 1).map(&:id))
+      elsif type == "user"
+        issues_from = issues(date_new).where(:assigned_to_id => d)
+      end
+#      number[:new] << issues_from.where(:status => 1).count
+      number[:ongoing] << issues_from.count
+      number[:closed] << issues_from.where(:status => [3,4,6]).count
+      number[:datax] << date_new.strftime('%Y-%m')
+    end
+      number
   end
 
   def depIssuesNumber(issues_from)
@@ -98,8 +117,8 @@ class SmartChartsController < ApplicationController
     number[:new], number[:ongoing], number[:closed] = [], [], [] 
     for group_id in Setting.plugin_smart_chart['department_ids'].split(",")
       issues_to = issues_from.where(:assigned_to_id => Group.find(group_id).users.where(:status => 1).map(&:id))
-      number[:new] << issues_to.where(:status => 1).count
-      number[:ongoing] << issues_to.where(:status => [2,5]).count
+#      number[:new] << issues_to.where(:status => 1).count
+      number[:ongoing] << issues_to.count
       number[:closed] << issues_to.where(:status => [3,4,6]).count
     end
     number
@@ -108,23 +127,9 @@ class SmartChartsController < ApplicationController
   def depMember(dep)
     member = []
     Group.find_by_lastname(dep).users.each do |user|
-      member << %Q{<a href="#{issues_url(:set_filter => 1, :assigned_to_id => user.id)}">"#{user.lastname}"</a>} if user.active?
+      member << user if user.active?
     end
     member
-  end
-
-  def depMemberIssuesNumber(dep, issues_from)
-    number = Hash.new
-    number[:new], number[:ongoing], number[:closed] = [], [], []
-    Group.find_by_lastname(dep).users.map(&:id).each do |id|
-      if User.find(id).active?
-        issues_to = issues_from.where(:assigned_to_id => id)
-        number[:new] << issues_to.where(:status => 1).count
-        number[:ongoing] << issues_to.where(:status => [2,5]).count
-        number[:closed] << issues_to.where(:status => [3,4,6]).count
-      end
-    end
-    number
   end
 
   def checkPermission
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@
 # See: http://guides.rubyonrails.org/routing.html
 get 'smart_charts',  :to => 'smart_charts#index'
 get 'smart_charts/show',  :to => 'smart_charts#show'
+get 'smart_charts/show-departments',  :to => 'smart_charts#showDepartments'
+get 'smart_charts/show-details',  :to => 'smart_charts#showDetails'
 get 'smart_charts/top10',  :to => 'smart_charts#top10'
-get 'smart_charts/show_recent_month', :to => 'smart_charts#showRecentMonth'
+get 'smart_charts/show-recent-month', :to => 'smart_charts#showRecentMonth'
